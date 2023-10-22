f484aeedbf74ab2da08f9e09b4f9cf83a2396f26
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-10-23 00:27:26 +0900

    createad weather app

--- weather/App.js
+++ weather/App.js
@@ -1,21 +1,58 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "skyblue",
+    },
+
+    city: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+
+    cityName: {
+        // color: "white",
+        fontSize: 50,
+        fontWeight: "bold",
+    },
+
+    weather: {
+        flex: 4,
+    },
+
+    day: {
+        flex: 1,
+        justifyContent: "flex-start",
+        alignItems: "center",
+    },
+
+    temperature: {
+        marginTop: 20,
+        fontSize: 150,
+        fontWeight: "bold",
+    },
+    description: {
+        fontSize: 50,
+        fontWeight: "bold",
+        marginTop: -40,
+    },
+});
+
 export default function App() {
     return (
-        <View style={{ flex: 1 }}>
-            <View style={{ flex: 1, backgroundColor: "teal" }}></View>
-            <View
-                style={{
-                    flex: 1,
-                    backgroundColor: "orange",
-                    flexDirection: "row",
-                }}
-            >
-                <View style={{ flex: 1, backgroundColor: "blue" }}></View>
-                <View style={{ flex: 1, backgroundColor: "black" }}></View>
+        <View style={styles.container}>
+            <View style={styles.city}>
+                <Text style={styles.cityName}>Seoul</Text>
             </View>
-            <View style={{ flex: 1, backgroundColor: "pink" }}></View>
+            <View style={styles.weather}>
+                <View style={styles.day}>
+                    <Text style={styles.temperature}>27</Text>
+                    <Text style={styles.description}>Sunny</Text>
+                </View>
+            </View>
         </View>
     );
 }
