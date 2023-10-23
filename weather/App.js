eec89b6c5600d8db75294456366481ce112312b1
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-10-23 16:12:42 +0900

    bring 5days weather datas

--- weather/App.js
+++ weather/App.js
@@ -1,6 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { useEffect, useState } from "react";
-import { Dimensions, ScrollView, StyleSheet, Text, View } from "react-native";
+import {
+    ActivityIndicator,
+    Dimensions,
+    ScrollView,
+    StyleSheet,
+    Text,
+    View,
+} from "react-native";
 import * as Location from "expo-location";
 
 const { height: DISPLAY_HEIGHT, width: DISPLAY_WIDTH } =
@@ -13,13 +20,14 @@ const styles = StyleSheet.create({
     },
 
     city: {
-        flex: 1,
+        flex: 0.8,
         justifyContent: "center",
         alignItems: "center",
     },
 
     cityName: {
         // color: "white",
+        marginTop: 50,
         fontSize: 50,
         fontWeight: "bold",
     },
@@ -37,16 +45,25 @@ const styles = StyleSheet.create({
     },
 
     temperature: {
-        // marginTop: 50,
-        fontSize: 150,
+        fontSize: 100,
         fontWeight: "bold",
     },
     description: {
         fontSize: 50,
         fontWeight: "bold",
-        marginTop: -40,
+        marginTop: -20,
+    },
+    loading: {
+        fontSize: 20,
+        marginBottom: 10,
+    },
+    subDescription: {
+        fontSize: 20,
+        fontWeight: "bold",
     },
 });
+
+const API_KEY = "476871574759c85c661f9c6f56a5e0b7";
 
 export default function App() {
     const [city, setCity] = useState("Loading...");
@@ -71,10 +88,50 @@ export default function App() {
         );
 
         setCity(location[0].city);
+        getWeather(latitude, longitude);
+    }
+
+    async function getWeather(latitude, longitude) {
+        let weathers = [];
+        const res = await fetch(
+            `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+        );
+
+        const json = await res.json();
+        let cnt = 0;
+
+        if (json.list && forecast.length === 0) {
+            weathers.push({
+                temp: json.list[0].main.temp,
+                weather: json.list[0].weather[0].main,
+                description: json.list[0].weather[0].description,
+                date: json.list[0].dt_txt,
+            });
+
+            for (var weather of json.list.slice(1)) {
+                if (
+                    !weather.dt_txt
+                        .split(" ")[0]
+                        .includes(weathers.at(-1).date.split(" ")[0]) &&
+                    weather.dt_txt.split(" ")[1] === "00:00:00"
+                ) {
+                    weathers.push({
+                        temp: weather.main.temp,
+                        weather: weather.weather[0].main,
+                        description: weather.weather[0].description,
+                        date: weather.dt_txt,
+                    });
+                }
+            }
+
+            setForecast(weathers);
+        }
     }
 
     useEffect(() => {
-        if (getPermission()) getLocation();
+        if (getPermission()) {
+            getLocation();
+        }
     }, []);
 
     return (
@@ -88,10 +145,29 @@ export default function App() {
                 pagingEnabled
                 showsHorizontalScrollIndicator={false}
             >
-                <View style={styles.day}>
-                    <Text style={styles.temperature}>27</Text>
-                    <Text style={styles.description}>Sunny</Text>
-                </View>
+                {forecast.length !== 0 ? (
+                    forecast.map((weather, index) => (
+                        <View key={index} style={styles.day}>
+                            <Text style={styles.temperature}>
+                                {parseFloat(weather.temp).toFixed(1)}°C
+                            </Text>
+                            <Text style={styles.description}>
+                                {weather.weather}
+                            </Text>
+                            <Text style={styles.subDescription}>
+                                {weather.description}
+                            </Text>
+                            <Text style={styles.subDescription}>
+                                {weather.date.split(" ")[0]}
+                            </Text>
+                        </View>
+                    ))
+                ) : (
+                    <View style={styles.day}>
+                        <Text style={styles.loading}>Loading</Text>
+                        <ActivityIndicator size="large" />
+                    </View>
+                )}
             </ScrollView>
         </View>
     );
